7521d4c31cbbfc6342870eef1679a18f008555a2
sathish <sathish.jersy17@gmail.com>
2020-06-07 03:27:43 +0530

    initial commit

---
+++ JenkinsIntegration/src/test/java/JenkinsTests/JenkinTest.java
@@ -0,0 +1,16 @@
+package JenkinsTests;
+
+public class JenkinTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("How are you Ravi");
+		System.out.println("How are you Raj");
+		System.out.println("How are you Kumar");
+		System.out.println("How are you Muthu");
+		System.out.println("How are you Mari");
+		
+	}
+
+}
