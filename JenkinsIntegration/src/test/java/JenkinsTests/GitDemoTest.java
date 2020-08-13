eeb9452a43ef99d5a002d758954ac18b38248a68
SATHISHKUMAR SURLIYSAMY <sathish1986>
2020-08-13 11:58:24 -0400

    adding second scripts

---
+++ JenkinsIntegration/src/test/java/JenkinsTests/GitDemoTest.java
@@ -0,0 +1,14 @@
+package JenkinsTests;
+
+public class JenkinTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+      System.out.println("Learning GIT commands with GitHUB");
+      
+      
+		
+	}
+
+}
